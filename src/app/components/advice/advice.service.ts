16e137fada3d11a34dadd14827b310fb8e406288
Kagiso Hadebe <Hadebekagiso3@gmail.com>
2024-02-18 22:14:25 +0200

    [feature] Service Additons
    - added services

--- src/app/components/advice/advice.service.ts
+++ src/app/components/advice/advice.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AdviceService {
 
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) {}
+
+
+  //returns data by random
+  fetchData(): Observable<any> {
+    return this.http.get('https://api.adviceslip.com/advice');
+  }
+
+  //returns data by Id
+
+  fetchDataById(slipId : number) : Observable<any> {
+    return this.http.get(`https://api.adviceslip.com/advice/${slipId}`);
+  }
 }
+
+
